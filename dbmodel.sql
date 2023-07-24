e04d5aa6da79e91ca9166f9aac117f599e6f2864
Evan Pulgino <evan.pulgino@gmail.com>
2023-07-24 06:40:55 -0700

    remove comment example

--- dbmodel.sql
+++ dbmodel.sql
@@ -13,15 +13,6 @@
 -- already created and must not be created here
 -- Note: The database schema is created from this file when the game starts. If you modify this file,
 --       you have to restart a game to see your changes in database.
--- Example 1: create a standard "card" table to be used with the "Deck" tools (see example game "hearts"):
--- CREATE TABLE IF NOT EXISTS `card` (
---   `card_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
---   `card_type` varchar(16) NOT NULL,
---   `card_type_arg` int(11) NOT NULL,
---   `card_location` varchar(16) NOT NULL,
---   `card_location_arg` int(11) NOT NULL,
---   PRIMARY KEY (`card_id`)
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 CREATE TABLE IF NOT EXISTS `calendar_tile` (
     `calendar_tile_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
