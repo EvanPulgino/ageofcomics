20771092627590e283e37fe361d5f0e90a253832
Evan Pulgino <evan.pulgino@gmail.com>
2023-07-27 08:12:22 -0700

    Fix viewport scaling

--- src/GameBody.ts
+++ src/GameBody.ts
@@ -40,17 +40,23 @@ class GameBody extends GameBasics {
     var r = t.w;
     var s = 2772;
     var height = dojo.marginBox("aoc-layout").h;
+    var viewportWidth = dojo.window.getBox().w;
+    var gameAreaWidth = viewportWidth < 980 ? viewportWidth : viewportWidth - 245;
+
     if (r >= s) {
       var i = (r - s) / 2;
       dojo.style("aoc-gboard", "transform", "");
       dojo.style("aoc-gboard", "left", i + "px");
       dojo.style("aoc-gboard", "height", height + "px");
+      dojo.style("aoc-gboard", "width", gameAreaWidth + "px");
     } else {
       var o = r / s;
       i = (t.w - r) / 2;
+      var width = viewportWidth <= 245 ? gameAreaWidth : gameAreaWidth / o;
       dojo.style("aoc-gboard", "transform", "scale(" + o + ")");
       dojo.style("aoc-gboard", "left", i + "px");
       dojo.style("aoc-gboard", "height", height * o + "px");
+      dojo.style("aoc-gboard", "width", width + "px");
     }
   }
 
