51ceb458cdb0de35044731e004d9cb42d55359a0
Evan Pulgino <evan.pulgino@gmail.com>
2023-11-20 14:39:13 -0800

    Update Next Player Setup state

--- modules/states/AOCNextPlayerSetupState.class.php
+++ modules/states/AOCNextPlayerSetupState.class.php
@@ -11,6 +11,8 @@
  *
  * Backend functions used by the nextPlayerSetup State
  *
+ * During this game state the number of ideas and extra money a player gets is determined based on their starting player order.
+ *
  * @EvanPulgino
  */
 
@@ -21,30 +23,53 @@ class AOCNextPlayerSetupState {
         $this->game = $game;
     }
 
-    function getArgs() { return []; }
+    /**
+     * Gets the list of args used by the NextPlayerSetup state
+     *
+     * Args:
+     *  - None
+     */
+    function getArgs() {
+        return [];
+    }
 
+    /**
+     * This is called upon entering the nextPlayerSetup state.
+     * It sets how many ideas and how much extra money a player gets based on their starting player order.
+     *
+     * @return void
+     */
     function stNextPlayerSetup() {
+        // Get the active player
         $activePlayer = $this->game->playerManager->getActivePlayer();
+
+        // If the active player is the last player in turn order, end player setup
         if (
             $this->game->playerManager->isLastPlayerInTurnOrder($activePlayer)
         ) {
             $this->game->gamestate->nextState("endPlayerSetup");
         } else {
+            // Otherwise, activate the next player and set the number of ideas and extra money they get
             $this->game->playerManager->activateNextPlayer();
             $activePlayer = $this->game->playerManager->getActivePlayer();
             switch ($activePlayer->getTurnOrder()) {
                 case 1:
+                    // First player gets 2 ideas
                     $this->game->setGameStateValue(START_IDEAS, 2);
                     break;
                 case 2:
+                    // Second player gets 3 ideas
                     $this->game->setGameStateValue(START_IDEAS, 3);
                     break;
                 case 3:
+                    // Third player gets 2 ideas and 1 extra money
                     $this->game->setGameStateValue(START_IDEAS, 2);
                     $this->game->playerManager->adjustPlayerMoney(
                         $activePlayer->getId(),
                         1
                     );
+
+                    // Notify all players of the extra money
                     $this->game->notifyAllPlayers(
                         "setupMoney",
                         clienttranslate(
@@ -58,11 +83,14 @@ class AOCNextPlayerSetupState {
                     );
                     break;
                 case 4:
+                    // Fourth player gets 3 ideas and 1 extra money
                     $this->game->setGameStateValue(START_IDEAS, 3);
                     $this->game->playerManager->adjustPlayerMoney(
                         $activePlayer->getId(),
                         1
                     );
+
+                    // Notify all players of the extra money
                     $this->game->notifyAllPlayers(
                         "setupMoney",
                         clienttranslate(
@@ -76,6 +104,8 @@ class AOCNextPlayerSetupState {
                     );
                     break;
             }
+
+            // Go to player setup state for the next player
             $this->game->gamestate->nextState("nextPlayerSetup");
         }
     }
