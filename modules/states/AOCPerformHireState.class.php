13471002ae9649036802377ce2da193ec4224607
Evan Pulgino <evan.pulgino@gmail.com>
2023-11-22 13:53:01 -0800

    Cleanup perform hire state

--- modules/states/AOCPerformHireState.class.php
+++ modules/states/AOCPerformHireState.class.php
@@ -11,6 +11,8 @@
  *
  * Backend functions used by the performHire State
  *
+ * In this state, players can hire creatives from the market. Players mus hire one artist and one writer.
+ *
  * @EvanPulgino
  */
 
@@ -51,20 +53,21 @@ class AOCPerformHireState {
         ];
     }
 
-    function hireCreative($args) {
+    function hireCreative($cardId, $creativeType) {
         $activePlayerId = $this->game->getActivePlayerId();
         $activePlayer = $this->game->playerManager->getPlayer($activePlayerId);
-        $cardId = $args[0];
-        $creativeType = $args[1];
+
         $cardTypeId =
             $creativeType == "artist" ? CARD_TYPE_ARTIST : CARD_TYPE_WRITER;
 
+        // Draw the chosen card into the player's hand
         $card = $this->game->cardManager->drawCard(
             $activePlayerId,
             $cardId,
             $cardTypeId
         );
 
+        // If the card is value 1, gain a matching idea
         if ($card->getIdeas() == 1) {
             $this->game->playerManager->gainIdeaFromHiringCreative(
                 $activePlayer,
@@ -72,6 +75,7 @@ class AOCPerformHireState {
             );
         }
 
+        // Notify all players of the hire (with facedown card), and notify the active player privately (so they can see the card they hired)
         $this->game->notifyAllPlayers(
             "hireCreative",
             clienttranslate(
@@ -100,29 +104,34 @@ class AOCPerformHireState {
             ]
         );
 
+        // If the player hired an artist, set the CAN_HIRE_ARTIST game state value to 0
         if ($creativeType == "artist") {
             $this->game->setGameStateValue(CAN_HIRE_ARTIST, 0);
         }
 
+        // If the player hired a writer, set the CAN_HIRE_WRITER game state value to 0
         if ($creativeType == "writer") {
             $this->game->setGameStateValue(CAN_HIRE_WRITER, 0);
         }
 
+        // If the player has hired both an artist and a writer, move to the next state
         if (
             $this->game->getGameStateValue(CAN_HIRE_ARTIST) == 0 &&
             $this->game->getGameStateValue(CAN_HIRE_WRITER) == 0
         ) {
-            $queryParams = [
-                "card_owner" => $this->game->getActivePlayerId(),
-                "NOT card_location" => LOCATION_PLAYER_MAT,
-            ];
-            $cardsInHand = $this->game->cardManager->findCards($queryParams);
-            if (count($cardsInHand) > 6) {
+            // If the player has more than 6 cards in their hand, move to the discardCards state
+            // Otherwise, move to the nextPlayerTurn state
+            if (
+                $this->game->cardManager->getCountForHandSizeCheck(
+                    $activePlayerId
+                ) > 6
+            ) {
                 $this->game->gamestate->nextState("discardCards");
             } else {
                 $this->game->gamestate->nextState("nextPlayerTurn");
             }
         } else {
+            // If the player has not hired both an artist and a writer, move to the performNextHire state
             $this->game->gamestate->nextState("performNextHire");
         }
     }
